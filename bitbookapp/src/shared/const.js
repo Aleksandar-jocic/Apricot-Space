9ec81e8c4920c4f1b47d74b1538a78200f7c4ad9
brankolek <brankolek@gmail.com>
2018-06-12 12:12:58 +0200

    “const“

--- bitbookapp/src/shared/const.js
+++ bitbookapp/src/shared/const.js
@@ -1,5 +1,18 @@
 const url = "http://bitbookapi.azurewebsites.net/api"
+const headers = {
+
+
+
+    "Content-Type": "application/json",
+    "Key": "bitbookdev",
+    "SessionId": "2990B489-DB94-4AC1-ACDE-CDC9CC3EAEAE"
+
+
+}
+
+
 
 export {
-    url
+    url,
+    headers
 }
