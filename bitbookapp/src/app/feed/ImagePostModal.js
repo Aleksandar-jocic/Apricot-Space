6115a181f16952d567a037e8e3ef4a65dd1967a8
brankolek <brankolek@gmail.com>
2018-06-13 15:59:24 +0200

    image/textModal&Links

---
+++ bitbookapp/src/app/feed/ImagePostModal.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Modal from "react-modal";
+import postService from "../../services/postService"
+
+class ImagePostModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            newImagePost: ""
+        };
+    }
+
+    handleNewImagePost = (event) => {
+
+        this.setState({
+
+            newImagePost: event.target.value
+
+        })
+
+    }
+    uploadImagePost = () => {
+
+        postService.uploadImagePost({ "imageUrl": this.state.newImagePost }).then(() => {
+
+            this.props.getUpdatedPosts()
+        })
+
+
+
+
+    }
+    render() {
+        return (
+            <Modal
+                isOpen={this.props.imagePostModal}
+                contentLabel="Post new TextPost">
+                <h1>NEW IMAGE POST</h1>
+                <input type="text" onChange={this.handleNewImagePost} />
+                <br />
+                <br />
+                <br />
+                <button onClick={this.props.closeModal} >close Image Modal</button>
+                <br />
+                <br />
+                <br />
+                <button onClick={this.uploadImagePost} >Upload Image Post</button>
+            </Modal>
+        );
+    }
+}
+
+export default ImagePostModal;
