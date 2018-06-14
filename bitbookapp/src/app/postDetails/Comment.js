e4cc95b0c84a51d34d2223f00b4984566227f53e
brankolek <brankolek@gmail.com>
2018-06-14 10:47:49 +0200

    userPic/userName

--- bitbookapp/src/app/postDetails/Comment.js
+++ bitbookapp/src/app/postDetails/Comment.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import userService from "../../services/userService"
 
 class Comment extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+
+            avatarUrl: ""
+        };
+    }
+    componentDidMount() {
+
+
+        userService.getUser(this.props.comment.authorId).then((user) => {
+
+            this.setState({
+
+
+                avatarUrl: user.avatarUrl
+            })
+        })
     }
     render() {
         return (
-            <div>{this.props.comment.body}</div>
+            <div>
+                {this.props.comment.body}
+                <br />
+                <span>Author name: {this.props.comment.authorName}</span>
+                <br />
+                <img width="320px" src={this.state.avatarUrl} alt="" />
+            </div>
         );
     }
 }
--- bitbookapp/src/shared/const.js
+++ bitbookapp/src/shared/const.js
@@ -8,9 +8,19 @@ const headers = {
 
 
 }
+const headersImg = {
+
+
+
+    "Key": "bitbookdev",
+    "SessionId": "2990B489-DB94-4AC1-ACDE-CDC9CC3EAEAE"
+
+
+}
 
 
 export {
     url,
-    headers
+    headers,
+    headersImg
 }
