cf1e7d99e61cc1b006c141fc65ed2afaaeae9d73
brankolek <brankolek@gmail.com>
2018-06-15 13:45:41 +0200

    NothingInFeed

--- bitbookapp/src/app/profile/ProfilePage.js
+++ bitbookapp/src/app/profile/ProfilePage.js
@@ -38,14 +38,23 @@ class ProfilePage extends React.Component {
         })
     }
 
-    componentDidMount() {
-        userService.getProfile().then((profile) => {
-            console.log(profile);
 
-            this.setState({
-                profile
-            })
-        })
+    componentDidMount() {
+        this.props.match.params.id ?
+            (userService.getUser(this.props.match.params.id).then((profile) => {
+                console.log(profile);
+
+                this.setState({
+                    profile
+                })
+            }))
+            : (userService.getProfile().then((profile) => {
+                console.log(profile);
+
+                this.setState({
+                    profile
+                })
+            }))
 
 
     }
