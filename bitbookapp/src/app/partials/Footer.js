f07e199198e5b5989b82e1e5a4cf4afc2432286a
student <student@bgit.rs>
2018-06-19 16:17:52 +0200

    minor improvements

---
+++ bitbookapp/src/app/partials/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+
+    <div id='footer'>
+        <span>copyright <i class="far fa-copyright"></i> team Aleksandar and Branko</span>
+    </div>
+)
+export default Footer;
